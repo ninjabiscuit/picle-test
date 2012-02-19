07d76bd703d641e5c4fd0acc144bed1093effecb
Andrew Walker <andy@madebymany.co.uk>
2012-02-19 11:58:28 +0000

    instructional stuff

--- player.js
+++ player.js
@@ -62,6 +62,7 @@ if (!PICLE) {
 		destroy : function() {
 			soundManager.stopAll();
 			soundManager.reboot();
+			this.resources = null;
 			this.scroller.destroy();
 			this.pauseBtn.off("click").remove();
 			$(document).off("keydown");
@@ -221,23 +222,4 @@ if (!PICLE) {
 	
 })(window, document, window.jQuery);
 
-var resources = [
-  {name:"Pluck",      sound:"media/Andy/01", img:"media/Andy/01.jpg" },
-  {name:"Lightning",  sound:"media/Andy/02", img:"media/Andy/02.jpg" },
-  {name:"Cabin",      sound:"media/Andy/03", img:"media/Andy/03.jpg" },
-  {name:"Tone",       sound:"media/Andy/04", img:"media/Andy/04.jpg" },
-  {name:"Rats",       sound:"media/Andy/05", img:"media/Andy/05.jpg" },
-  {name:"Gulls",      sound:"media/Andy/06", img:"media/Andy/06.jpg" },
-  {name:"Music",      sound:"media/Andy/07", img:"media/Andy/07.jpg" },
-  {name:"blah1",      sound:"media/Andy/08", img:"media/Andy/08.jpg" },
-  {name:"blah2",      sound:"media/Andy/09", img:"media/Andy/09.jpg" },
-  {name:"blah3",      sound:"media/Andy/10", img:"media/Andy/10.jpg" }
-];
 
-var player = new PICLE.Player();
-
-player.init($("#player"), resources);
- 
-soundManager.flashVersion = 9; // optional: shiny features (default = 8)
-soundManager.useHTML5Audio = true;
-soundManager.debugMode = false;
